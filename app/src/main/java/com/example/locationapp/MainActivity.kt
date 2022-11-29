d7a38afaf2723ae7fdd63f0f73a156be8440f619
Salmberg <60116309+Salmberg@users.noreply.github.com>
2022-11-29 11:12:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.locationapp
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_LOCATION = 1
+    lateinit var locationProvider: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.Builder(2000).build()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    Log.d("!!!", "lat: ${location.latitude}, lng: ${location.longitude}")
+                }
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION
+                )
+        }
+    }
+
+    override fun onResume(){
+        super.onResume()
+        startLocationUpdate()
+    }
+
+    override fun onPause(){
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    fun stopLocationUpdates(){
+        locationProvider.removeLocationUpdates(locationCallback)
+    }
+
+    fun startLocationUpdate (){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+            locationProvider.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdate()
+                // Börja använda position
+            }
+        }
+    }
+}
